3091f2710ca32392446d7a69ee453a9689506cbf
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2024-12-08 00:03:59 +0700

    feat: mir page

--- src/frontend/app/components/navbar.tsx
+++ src/frontend/app/components/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Navbar({theme = "default"}: {theme?: string}) { //  theme bisa hmif atau ukm, kalau ada di homepage dia default, bisa dimanfaatin buat logic milih warna
+export default function Navbar() {
   return (
     <div className='bg-blue'>Ini Navbar</div>
     )
