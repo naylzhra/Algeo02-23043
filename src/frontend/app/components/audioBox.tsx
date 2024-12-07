a21fc3ff261678e0d7cb6e23688deb499cb6c3a3
maymilla <164962489+MaylaYL@users.noreply.github.com>
2024-12-08 04:20:51 +0700

    add: audiobox, try integrating IIR

---
+++ src/frontend/app/components/audioBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AudioBox() {
+  return (
+    <div>
+        <div className="w-full h-[200px] bg-white-50 rounded-lg flex items-center justify-center"></div>
+        <p className="text-sm text-white-25">Audio name</p>
+    </div>
+    )
+}
+
--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import ImageRetrieval from "./components/image";
+import SideBar from "./components/sidebar";
+import AudioBox from "./components/audioBox";
 
 export default function Main() {
   return (
     <div className="w-full h-full">
       <ImageRetrieval/>
+      <AudioBox/>
     </div>
   )
 }
