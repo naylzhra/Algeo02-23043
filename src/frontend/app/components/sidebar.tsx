40271ab2628b861e0ed97ff40b7c737262876b9a
Nayla Zahira <19623201@std.stei.itb.ac.id>
2024-12-08 13:04:51 +0700

    feat: add start button on sidebar component

---
+++ src/frontend/app/components/sidebar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function SideBar(){
+    return(
+        <div className="bg-opacity-40 bg-white-25 h-[80vh] rounded-[23px] p-6 flex flex-col items-center gap-2">
+            <div className="w-full h-[200px] bg-white-50 rounded-lg flex items-center justify-center">
+                <span className="text-black font-medium">upload humming</span>
+            </div>
+            <p className="text-sm text-white-25">Query: yyyy.zip</p>
+            <button className="text-black font-bold mt-5 w-full bg-yellow-25 hover:bg-amber-500 py-2 rounded-lg font-medium">
+                START
+            </button>
+            <button className="text-white-25 mt-4 w-full bg-pink-25 hover:bg-red-500 py-2 rounded-lg font-medium">
+                Dataset Audio
+            </button>
+            <p className="text-sm text-white-25">Song: xxxx.zip</p>
+            <button className="text-white-25 mt-1 w-full bg-pink-25 hover:bg-red-500 py-2 rounded-lg font-medium">
+                Dataset Image
+            </button>
+            <p className="text-sm text-white-25">Image: xxxx.zip</p>
+            <button className="text-white-25 mt-1 w-full bg-pink-25 hover:bg-red-500 py-2 rounded-lg font-medium">
+                Mapper
+            </button>
+            <p className="text-sm text-white-25">Mapper: xxxx.json</p>
+        </div>
+    )
+}
--- src/frontend/tailwind.config.ts
+++ src/frontend/tailwind.config.ts
@@ -9,8 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "blue": {
+          25: "#173047",
+          50: "#6C8DA3",
+        },
+        "pink": {
+          25: "#FF7E79",
+        },
+        "yellow": {
+          25: "#FFB61D",
+        },
+        "white": {
+          25: "#FFFFFF",
+          50: "#D9D9D9",
+        }
       },
     },
   },
