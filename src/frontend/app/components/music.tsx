9f38d35bbcdc2e5e4219d751a3a163a72ade5695
Nayla Zahira <19623201@std.stei.itb.ac.id>
2024-12-11 21:50:44 +0700

    feat: add router

---
+++ src/frontend/app/components/music.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import SideBar from "./sidebar";
+import { useRouter } from "next/navigation";
+
+export default function MusicRetrieval(){
+    const router = useRouter()
+    return(
+        <div className="flex flex-col">
+            {/*Navigation Bar*/}
+            <div className="flex flex-row border align-middle justify-between mt-3">
+                {/*Left Component*/}
+                <div className="flex flex-row gap-5 ml-7"> 
+                    <button>
+                        <img src="\Home 3.svg" alt="" className="w-6 h-6 pb-0.5"/>
+                    </button>
+                    <button>
+                        <img src="\MuseIc.svg" alt="" className="w-20"/>
+                    </button>
+                </div>
+                {/*Right Components*/}
+                <div className="flex flex-row align-middle justify-items-center">
+                    <button className="button-navbar-on">
+                        <img src="\Music Equalizer.svg" alt="" className="w-5 h-5" />
+                        <span className="text-white"> Music Retrieval </span>
+                    </button>
+                    <button
+                        onClick={()=>router.push("/album_finder")}
+                        className="button-navbar-off">
+                        <img src="\Music Folder Song.svg" alt="" className="w-5 h-5" />
+                        <span className="text-white"> Album Finder </span>
+                    </button>
+                </div>
+            </div>
+            {/*Body*/}
+            <div className="grid grid-cols-5 gap-8 ml-7 mr-7 mt-10">
+                {/*Side Bar*/}
+                <SideBar/>
+                {/*Main Page Bar*/}
+                <div className="border col-span-4">
+                    Main Container
+                </div>
+            </div>
+        </div>
+        
+    )
+}
--- src/frontend/app/components/navbar.tsx
+++ src/frontend/app/components/navbar.tsx
@@ -1,7 +1,36 @@
+"use client";
+
 import React from 'react'
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const router = useRouter()
   return (
-    <div className='bg-blue'>Ini Navbar</div>
-    )
+      <div className="flex flex-row border align-middle justify-between mt-3">
+          {/*Left Component*/}
+          <div className="flex flex-row gap-5 ml-7"> 
+              <button>
+                  <img src="\Home 3.svg" alt="" className="w-6 h-6 pb-0.5"/>
+              </button>
+              <button>
+                  <img src="\MuseIc.svg" alt="" className="w-20"/>
+              </button>
+          </div>
+          {/*Right Components*/}
+          <div className="flex flex-row align-middle justify-items-center">
+              <button 
+                  onClick={()=>router.push("/music_retrieval")}
+                  className="button-navbar-off">
+                  <img src="\Music Equalizer.svg" alt="" className="w-5 h-5" />
+                  <span className="text-white"> Music Retrieval </span>
+              </button>
+              <button
+                  onClick={()=>router.push("/album_finder")}
+                  className="button-navbar-off">
+                  <img src="\Music Folder Song.svg" alt="" className="w-5 h-5" />
+                  <span className="text-white"> Album Finder </span>
+              </button>
+          </div>
+      </div>
+  )
 }
